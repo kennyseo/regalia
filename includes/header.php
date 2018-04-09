76e330b4a7c0270dd70e31923e310dbcb387f997
kennyseo <kenny.seo@websitedevteam.com>
2018-04-08 20:51:48 -0500

    layout done

--- includes/header.php
+++ includes/header.php
@@ -1,48 +1,17 @@
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>University of Arkansas Regalia Management System</title>
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://shop.uofastore.com//fonts.googleapis.com/css?family=Lato:400,400i,700,700i|Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
 
-
-<title>University of Arkansas Bookstore and Razorback Shop</title>
-<meta name="description" content="The University of Arkansas Bookstore is constantly working to provide students with the best possible pricing on textbooks, school supplies, computers and academic materials. Don’t forget to get your Razorbacks Accessories, including hats and caps, diploma frames and graduation salute." />
-<meta name="keywords" content="University of Arkansas, Bookstore, Textbooks, Razorbacks, Hogs, Fayetteville, Northwest Arkansas, NWA, General Books, Study Aids, Computers, mp3 players, iPads, Supplies, U of A, Garland Center" />
-
-
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="https://shop.uofastore.com//fonts.googleapis.com/css?family=Lato:400,400i,700,700i|Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
-
-<!--Xmlpackage 'script.head' -->
-<script>
-						(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-						(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-						m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-						})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-						ga('create', 'UA-68001224-3', 'shop.uofastore.com');
-
-						ga('send', 'pageview');
-					</script>
-<!--End of xmlpackage 'script.head' -->
-
-
-
-<script src="https://shop.uofastore.com/scripts/_skin_bundle_b54953e000cbf9949fb6c3a055b62bd9?v=rmK0qSj3J5HLW5T2nHBRg819_71C8hStCGnvCmqfOp81"></script>
-
-
-
-
-<link href="https://shop.uofastore.com/skins/skin_1/css/_skin_bundle?v=TazmYgBKT4f_tAuJpFpycJQZ57pVQXBaVZGh_aeSE9U1" rel="stylesheet"/>
-
-
-
-
-
-
-<link href="https://shop.uofastore.com/skins/skin_1/css/bundledhome?v=K9zpE7pfT1p7mfLLH4jp8Yeh_WxsLAYrUQ5tJ0T41wg1" rel="stylesheet"/>
-
+  <script src="https://shop.uofastore.com/scripts/_skin_bundle_b54953e000cbf9949fb6c3a055b62bd9?v=rmK0qSj3J5HLW5T2nHBRg819_71C8hStCGnvCmqfOp81"></script>
+  <link href="https://shop.uofastore.com/skins/skin_1/css/_skin_bundle?v=TazmYgBKT4f_tAuJpFpycJQZ57pVQXBaVZGh_aeSE9U1" rel="stylesheet"/>
+  <link href="css/custom.css" rel="stylesheet"/>
 
 </head>
-
 <body>
